c42dbab72616eec22041e34c37bbbd2215210217
Floyd Everest <floyd.everest@monash.edu>
2026-07-13 10:48:16 +1000

    Refactored experiments.

---
+++ experiments/grand_prix.R
@@ -0,0 +1,244 @@
+grand_prix_plan <- function() {
+  list(
+    # -------------------------------------------------------------------------
+    # Compute the RIPr (optimal P_W) for each Q alternative and batch size.
+    # This is the most computationally intensive step, so we do it first and on
+    # the GPU if available. The resulting P_W's are then used in the BatchRIPr
+    # target below, which runs on the CPU but is much faster.
+    # -------------------------------------------------------------------------
+
+    tar_target(
+      max_n,
+      100L
+    ),
+
+    tar_target(n_vals, as.list(seq_len(max_n))),
+
+    # # Alternative distributions Q used as the numerator in each martingale.
+    tar_target(
+      sim_Q,
+      {
+        box::use(ripr / mixture[discrete_simplex_mixture, truncated_dirichlet])
+        list(
+          list(
+            name = "point_754",
+            K = 3L,
+            Q = discrete_simplex_mixture(as.matrix(c(7 / 16, 5 / 16, 4 / 16)), 1)
+          ),
+          list(
+            name = "dirichlet_754",
+            K = 3L,
+            Q = truncated_dirichlet(c(7, 5, 4))
+          ),
+          list(
+            name = "point_855",
+            K = 3L,
+            Q = discrete_simplex_mixture(as.matrix(c(8 / 18, 5 / 18, 5 / 18)), 1)
+          ),
+          list(
+            name = "dirichlet_855",
+            K = 3L,
+            Q = truncated_dirichlet(c(8, 5, 5))
+          ),
+          list(
+            name = "point_7531",
+            K = 4L,
+            Q = discrete_simplex_mixture(
+              as.matrix(c(7 / 16, 5 / 16, 3 / 16, 1 / 16)),
+              1
+            )
+          ),
+          list(
+            name = "dirichlet_7531",
+            K = 4L,
+            Q = truncated_dirichlet(c(7, 5, 3, 1))
+          ),
+          list(
+            name = "point_8444",
+            K = 4L,
+            Q = discrete_simplex_mixture(
+              as.matrix(c(8 / 20, 4 / 20, 4 / 20, 4 / 20)),
+              1
+            )
+          ),
+          list(
+            name = "dirichlet_8444",
+            K = 4L,
+            Q = truncated_dirichlet(c(8, 4, 4, 4))
+          )
+        )
+      }
+    ),
+
+    tar_target(
+      sim_ripr_optimal,
+      {
+        q_opt <- sim_Q[[1L]]
+        n <- n_vals[[1L]]
+        result <- run_plurality_ripr(
+          n = n,
+          q = q_opt$Q,
+          fw_iters = 25L,
+          em_iters = 10L,
+          verbose = TRUE
+        )
+        list(
+          Q_name = q_opt$name,
+          K = q_opt$K,
+          Q = q_opt$Q,
+          P_W = result$mixture,
+          e_ratio = result$gap
+        )
+      },
+      pattern = cross(sim_Q, n_vals),
+      iteration = "list"
+    ),
+
+    # -------------------------------------------------------------------------
+    # Grand Prix: comparison of UITest, InfimumMartingale, BatchRIPr martingales.
+    # -------------------------------------------------------------------------
+
+    # Scenarios: true data-generating distributions.
+    tar_target(
+      sim_scenario,
+      list(
+        list(name = "point_754",  q = c(7 / 16, 5 / 16, 4 / 16), K = 3L),
+        list(name = "point_855",  q = c(8 / 18, 5 / 18, 5 / 18), K = 3L),
+        list(name = "point_7531", q = c(7 / 16, 5 / 16, 3 / 16, 1 / 16), K = 4L),
+        list(name = "point_8444", q = c(8 / 20, 4 / 20, 4 / 20, 4 / 20), K = 4L)
+      )
+    ),
+
+    tar_target(sim_reps, 1000L),
+
+    tar_target(
+      sim_seqs,
+      c(
+        sim_scenario[[1L]],
+        list(
+          data = replicate(
+            sim_reps,
+            sample(
+              seq_len(sim_scenario[[1L]]$K),
+              size = max_n,
+              replace = TRUE,
+              prob = sim_scenario[[1L]]$q
+            )
+          )
+        )
+      ),
+      pattern = map(sim_scenario),
+      iteration = "list"
+    ),
+
+    tar_target(
+      universal_inference,
+      {
+        ss <- sim_seqs
+        lapply(
+          Filter(function(sq) sq$K == ss$K, sim_Q),
+          function(sq) {
+            list(
+              Q_name = sq$name,
+              dgp_name = ss$name,
+              values = run_martingale(UITest(sq$Q, 0.05), ss$data)
+            )
+          }
+        )
+      },
+      pattern = map(sim_seqs),
+      iteration = "list"
+    ),
+
+    tar_target(
+      infimum_martingale,
+      {
+        ss <- sim_seqs
+        lapply(
+          Filter(function(sq) sq$K == ss$K, sim_Q),
+          function(sq) {
+            list(
+              Q_name = sq$name,
+              dgp_name = ss$name,
+              values = run_martingale(InfimumMartingale(sq$Q, 0.05), ss$data)
+            )
+          }
+        )
+      },
+      pattern = map(sim_seqs),
+      iteration = "list"
+    ),
+
+    tar_target(
+      ripr_sequence,
+      {
+        ss <- sim_seqs
+        lapply(
+          Filter(function(sq) sq$K == ss$K, sim_Q),
+          function(sq) {
+            q_name <- sq$name
+            ripr_fn <- function(n) {
+              entry <- Filter(
+                function(r) r$Q_name == q_name && r$P_W@n == n,
+                sim_ripr_optimal
+              )[[1L]]
+              list(mixture = entry$P_W, e_ratio = entry$e_ratio)
+            }
+            list(
+              Q_name = sq$name,
+              dgp_name = ss$name,
+              values = run_martingale(RIPrSequence(sq$Q, ripr_fn, 0.05), ss$data)
+            )
+          }
+        )
+      },
+      pattern = map(sim_seqs),
+      iteration = "list"
+    ),
+
+    tar_target(
+      batch_sizes,
+      list(1L, 2L)
+    ),
+
+    tar_target(
+      batch_ripr,
+      {
+        ss <- sim_seqs
+        bs <- batch_sizes
+        lapply(
+          Filter(function(sq) sq$K == ss$K, sim_Q),
+          function(sq) {
+            ripr_entry <- Filter(
+              function(r) r$Q_name == sq$name && r$P_W@n == bs,
+              sim_ripr_optimal
+            )[[1L]]
+            list(
+              Q_name = sq$name,
+              dgp_name = ss$name,
+              batch_size = bs,
+              values = run_martingale(
+                BatchRIPr(sq$Q, ripr_entry$P_W, ripr_entry$e_ratio, 0.05),
+                ss$data
+              )
+            )
+          }
+        )
+      },
+      pattern = cross(sim_seqs, batch_sizes),
+      iteration = "list"
+    ),
+
+    tar_target(
+      sim_long,
+      build_sim_long(universal_inference, infimum_martingale, ripr_sequence, batch_ripr)
+    ),
+
+    tar_target(sim_plot_754,  plot_mean_martingales(sim_long, "point_754",  3L)),
+    tar_target(sim_plot_855,  plot_mean_martingales(sim_long, "point_855",  3L)),
+    tar_target(sim_plot_7531, plot_mean_martingales(sim_long, "point_7531", 4L)),
+    tar_target(sim_plot_8444, plot_mean_martingales(sim_long, "point_8444", 4L))
+
+    NULL   # remove when activating targets above
+  )
+}
